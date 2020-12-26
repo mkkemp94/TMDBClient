127e7572f584b3425724f2fd7858631ead2ee9dd
MKemp <mkemp@accu-time.com>
2020-12-26 18:51:21 -0500

    Display movie data in recycler view

--- app/src/main/java/com/mkemp/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/mkemp/tmdbclient/presentation/movie/MovieActivity.kt
@@ -3,9 +3,12 @@ package com.mkemp.tmdbclient.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mkemp.tmdbclient.R
 import com.mkemp.tmdbclient.databinding.ActivityHomeBinding
 import com.mkemp.tmdbclient.databinding.ActivityMovieBinding
@@ -18,6 +21,7 @@ class MovieActivity : AppCompatActivity()
     lateinit var factory: MovieViewModelFactory
     private lateinit var movieViewModel: MovieViewModel
     private lateinit var binding: ActivityMovieBinding
+    private lateinit var adapter: MovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -30,9 +34,33 @@ class MovieActivity : AppCompatActivity()
         movieViewModel = ViewModelProvider(this, factory)
             .get(MovieViewModel::class.java)
 
+        initRecyclerView()
+        displayPopularMovies()
+    }
+
+    private fun initRecyclerView()
+    {
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter = adapter
+    }
+
+    private fun displayPopularMovies()
+    {
+        binding.movieProgressBar.visibility = View.VISIBLE
         val responseLiveData = movieViewModel.getMovies()
         responseLiveData.observe(this, Observer {
-            Log.i("MYTAG", it.toString())
+            if (it != null)
+            {
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            }
+            else
+            {
+                binding.movieProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext, "No data available", Toast.LENGTH_LONG).show()
+            }
         })
     }
 }
