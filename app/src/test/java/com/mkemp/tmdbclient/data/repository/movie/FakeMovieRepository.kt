7ccce7c45283625d05d7a66be54ad2372a04a94e
MKemp <mkemp@accu-time.com>
2020-12-27 16:38:52 -0500

    Unit test view model classes with live data

---
+++ app/src/test/java/com/mkemp/tmdbclient/data/repository/movie/FakeMovieRepository.kt
@@ -0,0 +1,30 @@
+package com.mkemp.tmdbclient.data.repository.movie
+
+import com.mkemp.tmdbclient.data.model.movie.Movie
+import com.mkemp.tmdbclient.domain.repository.MovieRepository
+
+class FakeMovieRepository : MovieRepository
+{
+    private val movies = mutableListOf<Movie>()
+
+    init
+    {
+
+        movies.add(Movie(1, "overview1", "posterpath1", "date1", "title1"))
+        movies.add(Movie(2, "overview2", "posterpath2", "date2", "title2"))
+        movies.add(Movie(3, "overview3", "posterpath3", "date3", "title3"))
+    }
+
+    override suspend fun getMovies(): List<Movie>?
+    {
+        return movies
+    }
+
+    override suspend fun updateMovies(): List<Movie>?
+    {
+        movies.clear()
+        movies.add(Movie(3, "overview3", "posterpath3", "date3", "title3"))
+        movies.add(Movie(4, "overview4", "posterpath4", "date4", "title4"))
+        return movies
+    }
+}
